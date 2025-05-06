01799f337d1474143b09376167e4169bb7627381
Mohsin Ali <mohsin.naqvi@kwanso.com>
2025-05-06 15:35:59 +0500

    [VOYAG-123] my first commit

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -3,7 +3,9 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+    console.log('AppController');
+  }
 
   @Get()
   getData() {
