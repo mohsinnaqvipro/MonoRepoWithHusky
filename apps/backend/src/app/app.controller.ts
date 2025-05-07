23d72886011a2cb47e7316f34433a49a3938f574
Mohsin Ali <mohsin.naqvi@kwanso.com>
2025-05-07 11:57:28 +0500

    [VOYAG-123] my first commit

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {
-    console.log('AppController123455');
+    console.log('AppController123');
   }
 
   @Get()
